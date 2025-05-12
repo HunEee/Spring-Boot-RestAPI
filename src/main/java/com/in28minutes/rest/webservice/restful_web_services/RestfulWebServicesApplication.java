2f2fccaf858fc75a821a17386f0ffb9c42423d88
HunEee <asdf981001@naver.com>
2025-05-12 22:02:02 +0900

    Rest Api 세팅

--- src/main/java/com/in28minutes/rest/webservice/restful_web_services/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservice/restful_web_services/RestfulWebServicesApplication.java
@@ -2,6 +2,9 @@ package com.in28minutes.rest.webservice.restful_web_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,4 +13,15 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // 모든 것에 대한 활성화
+						.allowedMethods("*") // 모든 메서드 허용(GET,PUT,POST)
+						.allowedOrigins("http://localhost:3000") // 어디서 오는 요청인지
+						.allowCredentials(true);
+			}
+		};
+	}
 }
--- src/main/java/com/in28minutes/rest/webservice/restful_web_services/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/rest/webservice/restful_web_services/security/SpringSecurityConfiguration.java
@@ -12,9 +12,14 @@ public class SpringSecurityConfiguration {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
-		//1) 모든 요청이 인증되도록 함
-		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		  
+		//CORS 활성화
+        http.cors(withDefaults());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        
+		//1) 모든 요청이 인증되도록 함 => CORS 활성화를 위해 주석처리함
+		//http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        
 		//2) 기본인증: 웹페이지를 보여주지 않지만 팝업창으로 인증을 요청 
 		http.httpBasic(withDefaults());
 		//3) CSRF 해제
