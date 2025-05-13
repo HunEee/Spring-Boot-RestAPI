950aa6f8142aecd0a271276cd42e0c2bedb2661c
HunEee <asdf981001@naver.com>
2025-05-13 23:30:41 +0900

    React와연동하는 RestAPI 제작

---
+++ src/main/java/com/in28minutes/rest/webservice/restful_web_services/todo/TodoService.java
@@ -0,0 +1,55 @@
+package com.in28minutes.rest.webservice.restful_web_services.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.Valid;
+
+@Service
+public class TodoService {
+	
+	// 이거 초기화 안하면 왜 에러나는지?
+	private static List<Todo> todos = new ArrayList<>();
+	
+	private static int todosCount = 0;
+	
+	static {
+		todos.add(new Todo(++todosCount,"in28minutes","Learn AWS",LocalDate.now().plusYears(1),false));
+		todos.add(new Todo(++todosCount,"in28minutes","Learn DevOps",LocalDate.now().plusYears(2),false));
+		todos.add(new Todo(++todosCount,"in28minutes","Learn Full Stack",LocalDate.now().plusYears(3),false));
+	}
+	
+	public List<Todo> findByUsername(String username){
+		Predicate<? super Todo> predicate = todo -> todo.getUsername().equalsIgnoreCase(username);
+		return todos.stream().filter(predicate).toList();
+	}
+	
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done ) {
+		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+		todos.add(todo);
+	}
+	
+	
+	public void deleteById(int id) {
+		//람다 todo -> todo.getId() == id
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		todos.removeIf(predicate);
+	}
+
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
+	}
+
+	public void updateTodo(@Valid Todo todo) {
+		deleteById(todo.getId());
+		todos.add(todo);
+		
+	}
+	
+}
